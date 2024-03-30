67925144cba69e158defeeaa3eb01bddd1e25a6f
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-03-30 11:16:39 -0500

    add business logic to make search input work in Dashboard container

--- src/components/modules/user/dashboard/Dashboard.jsx
+++ src/components/modules/user/dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TextInput from "@ui/forms/textInput/TextInput";
 import { InputText } from "primereact/inputtext";
 import { useForm } from "react-hook-form";
@@ -9,30 +9,11 @@ import CategoryCard from "@ui/cards/categoryCard/CategoryCard";
 import "./dashboard.sass";
 
 const Dashboard = () => {
-  const {
-    control,
-    handleSubmit,
-    setValue,
-    reset,
-    getValues,
-    watch,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      searchWord: "",
-    },
-  });
-  console.log(modules);
-  const getFormErrorMessage = (fieldName) => {
-    return (
-      errors[fieldName] && (
-        <small className="p-error">{errors[fieldName]?.message}</small>
-      )
-    );
-  };
+  const [filteredModule, setFilteredModule] = useState({});
+  const filteredModules = modules.filter((module) =>
+    module.title.toLowerCase().includes(filteredModule)
+  );
 
-  // const filteredModules = modules.filter(module => module.title.toLowerCase().includes(filteredModule.toLowerCase()))
-  console.log(getValues);
   return (
     <div className="layout">
       <div className="main__content -dashboard-container">
@@ -40,44 +21,34 @@ const Dashboard = () => {
           <h4>Discover:</h4>
           <span className="p-input-icon-left">
             <i className="pi pi-search" />
-            <InputText 
-            className="p-inputtext"
-            placeholder="Search" />
+            <InputText
+              className="p-inputtext"
+              placeholder="Search"
+              onChange={(e) => {
+                setFilteredModule(e.target.value);
+              }}
+            />
           </span>
-
-          <TextInput
-            showIcon={true}
-            iconName="pi pi-search"
-            isRequired={false}
-            labelName="Search"
-            isEdit={true}
-            getFormErrorMessage={getFormErrorMessage}
-            control={control}
-            inputName="searchWord"
-            placeHolderText="Search"
-            width="100%"
-            showLabel={false}
-          />
         </div>
         <div className="category__grid -dashboard">
-          {modules.map((module, key) => {
-            return (
-              <Link to={module.link}>
-                <CategoryCard
-                  hoverBackgroundColor={module.hoverBackgroundColor}
-                  hoverIconColor={module.hoverIconColor}
-                  backgroundColor={module.backgroundColor}
-                  iconBackgroundColor={module.iconBackgroundColor}
-                  height={module.height}
-                  fontColor={module.fontColor}
-                  title={module.title}
-                  description={module.description}
-                  icon={module.icon}
-                  className={"-dashboard-card"}
-                />
-              </Link>
-            );
-          })}
+          {filteredModules.length > 0
+            ? filteredModules.map((module, key) => (
+                <Link to={module.link} key={key}>
+                  <CategoryCard
+                    hoverBackgroundColor={module.hoverBackgroundColor}
+                    hoverIconColor={module.hoverIconColor}
+                    backgroundColor={module.backgroundColor}
+                    iconBackgroundColor={module.iconBackgroundColor}
+                    height={module.height}
+                    fontColor={module.fontColor}
+                    title={module.title}
+                    description={module.description}
+                    icon={module.icon}
+                    className="-dashboard-card"
+                  />
+                </Link>
+              ))
+            : "No modules found."}
         </div>
       </div>
     </div>
