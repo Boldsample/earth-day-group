d1647d2a7cf9d056e7067080ade6432c3bcfdd9d
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-04-10 20:41:37 -0500

    add more styling to FormTwo component

--- src/components/register/registerCompany/RegisterCompany.jsx
+++ src/components/register/registerCompany/RegisterCompany.jsx
@@ -1,116 +1,130 @@
-import React from 'react'
-import { useForm } from "react-hook-form"
-import { Button } from "primereact/button"
-import { useEffect, useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Button } from "primereact/button";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import { createUser } from "@services/userServices"
-import { getUserData } from "@store/slices/usersSlice"
-import ProfilePhoto from "@ui/profilePhoto/ProfilePhoto"
-import { updateThankyou } from "@store/slices/globalSlice"
-import GoBackButton from "@ui/buttons/goBackButton/GoBackButton"
-import { TabView, TabPanel } from 'primereact/tabview';
-import FormOne from './FormOne'
-import FormTwo from './FormTwo'
+import { createUser } from "@services/userServices";
+import { getUserData } from "@store/slices/usersSlice";
+import ProfilePhoto from "@ui/profilePhoto/ProfilePhoto";
+import { updateThankyou } from "@store/slices/globalSlice";
+import GoBackButton from "@ui/buttons/goBackButton/GoBackButton";
+import { TabView, TabPanel } from "primereact/tabview";
+import FormOne from "./FormOne";
+import FormTwo from "./FormTwo";
 
 const RegisterCompany = () => {
-    const navigate = useNavigate()
-	const dispatch = useDispatch()
-	const [sending, setSending] = useState(false)
-	const user = useSelector((state) => state.users.userData);
-	const [photoFileBlob, setPhotoFileBlob] = useState(user?.picture)
-	const {
-		reset,
-		control,
-		handleSubmit,
-		formState: { errors },
-	} = useForm({
-		defaultValues: {
-			description: "",
-			phone: null,
-			location: "",
-			password: "",
-			companyName: user?.name,
-			email: user?.email,
-			picture: user?.picture,
-			password_confirmation: "",
-			termsConditionsChecked: false
-		}
-	})
-	
-	const getFormErrorMessage = (fieldName) => errors[fieldName] && <small className="p-error">{errors[fieldName]?.message}</small>
-	const onSubmit = async (data) => {
-		if(await createUser({...user, ...data})){
-			dispatch(getUserData())
-			dispatch(updateThankyou({
-				title: "Congrats!", 
-				link: "/dashboard/",
-				background: "image-1.svg",
-				button_label: "Go to dashboard",
-				content: "You’re all signed up! We send you a verification link send your provide email. Please verify your identity.",
-			}))
-		}
-	}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [sending, setSending] = useState(false);
+  const user = useSelector((state) => state.users.userData);
+  const [photoFileBlob, setPhotoFileBlob] = useState(user?.picture);
+  const {
+    reset,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      description: "",
+      phone: null,
+      location: "",
+      password: "",
+      companyName: user?.name,
+      email: user?.email,
+      picture: user?.picture,
+      password_confirmation: "",
+      termsConditionsChecked: false,
+    },
+  });
 
-	useEffect(() => {
-		// dispatch(getUsersList());
-		// dispatch(getUserData(5));
-	}, [])
+  const getFormErrorMessage = (fieldName) =>
+    errors[fieldName] && (
+      <small className="p-error">{errors[fieldName]?.message}</small>
+    );
+  const onSubmit = async (data) => {
+    if (await createUser({ ...user, ...data })) {
+      dispatch(getUserData());
+      dispatch(
+        updateThankyou({
+          title: "Congrats!",
+          link: "/dashboard/",
+          background: "image-1.svg",
+          button_label: "Go to dashboard",
+          content:
+            "You’re all signed up! We send you a verification link send your provide email. Please verify your identity.",
+        })
+      );
+    }
+  };
 
-	// const uploadInvoice = async (invoiceFile) => {
-	//   let formData = new FormData();
-	//   formData.append('invoiceFile', invoiceFile);
+  useEffect(() => {
+    // dispatch(getUsersList());
+    // dispatch(getUserData(5));
+  }, []);
 
-	//   const response = await fetch(`orders/${orderId}/uploadInvoiceFile`,
-	//     {
-	//       method: 'POST',
-	//       body: formData
-	//     },
-	//   );
-	// };
+  // const uploadInvoice = async (invoiceFile) => {
+  //   let formData = new FormData();
+  //   formData.append('invoiceFile', invoiceFile);
 
-	// const invoiceUploadHandler = ( event ) => {
+  //   const response = await fetch(`orders/${orderId}/uploadInvoiceFile`,
+  //     {
+  //       method: 'POST',
+  //       body: formData
+  //     },
+  //   );
+  // };
 
-	//   const fileReader = new FileReader();
-	//   fileReader.onload = (e) => {
-	//     uploadInvoice(e.target.result);
-	//   };
-	//   fileReader.readAsDataURL(file);
-	// };
+  // const invoiceUploadHandler = ( event ) => {
 
-	// console.log(getValues());
+  //   const fileReader = new FileReader();
+  //   fileReader.onload = (e) => {
+  //     uploadInvoice(e.target.result);
+  //   };
+  //   fileReader.readAsDataURL(file);
+  // };
 
-	return <div className="layout">
-		<img className="layout__background" src="/assets/register/image-2.svg" />
-		<div className="main__content halfwidth">
-			<form onSubmit={handleSubmit(onSubmit)}>
-			<TabView>
-				<TabPanel header='tab 1'>
-					<FormOne 
-						control={control}
-						getFormErrorMessage={getFormErrorMessage}
-						photoFileBlob={photoFileBlob}
-						setPhotoFileBlob={setPhotoFileBlob}
-					/>
-				</TabPanel>
-				<TabPanel header='tab 2'>
-				<FormTwo 
-						control={control}
-						getFormErrorMessage={getFormErrorMessage}
-						photoFileBlob={photoFileBlob}
-						setPhotoFileBlob={setPhotoFileBlob}
-					/>
-				</TabPanel>	
-			</TabView>
-                
-				<div className="p-field" style={{ marginBottom: "24px" }}>
-					<Button className="dark-blue fullwidth" label="Sign up" type="submit" />
-				</div>
-			</form>
-		</div>
-		<Link to="/register/categories/"><GoBackButton /></Link>
-	</div>
-}
+  // console.log(getValues());
 
-export default RegisterCompany
+  return (
+    <div className="layout">
+      <img className="layout__background" src="/assets/register/image-2.svg" />
+      <div className="main__content halfwidth">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <TabView>
+            <TabPanel>
+              <FormOne
+                control={control}
+                getFormErrorMessage={getFormErrorMessage}
+                photoFileBlob={photoFileBlob}
+                setPhotoFileBlob={setPhotoFileBlob}
+              />
+            </TabPanel>
+            <TabPanel>
+              <FormTwo
+                control={control}
+                getFormErrorMessage={getFormErrorMessage}
+                photoFileBlob={photoFileBlob}
+                setPhotoFileBlob={setPhotoFileBlob}
+              />
+            </TabPanel>
+          </TabView>
+
+          <div className="p-field" style={{ marginBottom: "24px" }}>
+            <Button
+              className="dark-blue fullwidth"
+              label="Sign up"
+              type="submit"
+            />
+          </div>
+        </form>
+      </div>
+      <Link to="/register/categories/">
+        <GoBackButton />
+      </Link>
+    </div>
+  );
+};
+
+export default RegisterCompany;
--- src/ui/cards/recycleMaterialCard/RecycleMaterialCard.jsx
+++ src/ui/cards/recycleMaterialCard/RecycleMaterialCard.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import './recycleMaterialCard.sass'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose, faCircle } from "@fortawesome/free-solid-svg-icons";
+import "./recycleMaterialCard.sass";
 
 const RecycleMaterialCard = () => {
   return (
-    <div className='recycle__card'>
-        <span>circle</span>
-        <div className="body__container">
-            <p>Paper</p>
-            <p>1kg:$2.5</p>
-        </div>
+    <div className="recycle__card">
+      <FontAwesomeIcon icon={faCircle} />
+      <div className="body__container">
+        <p>Paper</p>
+        <p>1kg:$2.5</p>
+      </div>
+      <div className="close__icon">
+        <FontAwesomeIcon icon={faClose} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RecycleMaterialCard
+export default RecycleMaterialCard;
