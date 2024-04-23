5542078e60653c0e9a996a631384a6c6088ff32b
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-04-23 18:53:21 -0500

    saveOffer business logic

--- src/services/offersServices.jsx
+++ src/services/offersServices.jsx
@@ -1,9 +1,9 @@
+import { saveJSON } from "@utils/useJSON";
 
 export const createOffer = async (data) => {
-	const response = saveJSON('offers', data)
-	//await API.post("/register", data)
-	if(response?.status == 404)
-		toast.error(response.status+': '+response.data.message)
-	return true
-}
-
+  const response = saveJSON("offers", data);
+  //await API.post("/register", data)
+  if (response?.status == 404)
+    toast.error(response.status + ": " + response.data.message);
+  return true;
+};
--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,28 @@
-import { defineConfig } from 'vite'
-import mkcert from 'vite-plugin-mkcert'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [
-		react(),
-		mkcert(),
-	],
-	server: {
-		port: 9000,
-		https: true,
-		host: 'localhost',
-	},
-	resolve: {
-		alias: {
-			'@ui': '/src/ui',
-			'@json': '/src/json',
-			'@store': '/src/store',
-			'@utils': '/src/utils',
-			'@styles': '/src/styles',
-			'@assets': '/src/assets',
-			'@services': '/src/services',
-			'@components': '/src/components',
-			'@modules': '/src/components/modules'
-		},
-	},
-	optimizeDeps: {
-		exclude: ['@vis.gl/react-google-maps']
-	},
-})
+  plugins: [react(), mkcert()],
+  server: {
+    port: 9000,
+    https: true,
+    host: "localhost",
+  },
+  resolve: {
+    alias: {
+      "@ui": "/src/ui",
+      "@json": "/src/json",
+      "@store": "/src/store",
+      "@utils": "/src/utils",
+      "@styles": "/src/styles",
+      "@assets": "/src/assets",
+      "@services": "/src/services",
+      "@components": "/src/components",
+      "@modules": "/src/components/modules",
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@vis.gl/react-google-maps"],
+  },
+});
