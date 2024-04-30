c89449a5ee7cf12f574a1222e4af908df228c95f
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-04-29 20:55:55 -0500

    refactor Nav component for dinamic rendering based on user type

--- src/ui/nav/Nav.jsx
+++ src/ui/nav/Nav.jsx
@@ -2,31 +2,81 @@ import React from "react";
 import "./nav.sass";
 import { Tooltip } from "primereact/tooltip";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Nav = () => {
-	const sections = [
-		{ label: "Home", icon: '/assets/icons/home.svg', path: "/dashboard/" },
-		{ label: "Location", icon: '/assets/icons/location.svg', path: "/location/" },
-		{ label: "Inbox", icon: '/assets/icons/inbox.svg', path: "/orders/" },
-		{ label: "Shop", icon: '/assets/icons/shop.svg', path: "" },
-		{ label: "Settings", icon: '/assets/icons/settings.svg', path: "/settings/" },
-	];
-	return <div className="nav__container">
-		{sections.map((section, key) => {
-			return <Link key={key} to={section.path}>
-				<Tooltip target=".custom-target-icon" />
-					<img
-					data-pr-tooltip={section.label}
-					data-pr-position="right"
-					data-pr-at="right+5 top"
-					data-pr-my="left center-2"
-					className="custom-target-icon"
-					key={section.label}
-					src={section.icon}
-					></img>
-			</Link>
-		})}
-	</div>
-}
+  const userRole = useSelector((state) => state.users.userData.role);
+  console.log(userRole, "role");
+  let sections = [];
 
-export default Nav
+  sections.push({
+    label: "Home",
+    icon: "/assets/icons/home.svg",
+    path: "/dashboard/",
+  });
+  if (userRole === "user") {
+    sections.push({
+      label: "Location",
+      icon: "/assets/icons/location.svg",
+      path: "/location/",
+    });
+    sections.push({
+      label: "Inbox",
+      icon: "/assets/icons/inbox.svg",
+      path: "/orders/",
+    });
+    sections.push({
+      label: "Shop",
+      icon: "/assets/icons/shop.svg",
+      path: "",
+    });
+  }
+  if (userRole === "company") {
+    sections.push({
+      label: "Profile",
+      icon: "/assets/icons/userProfile.svg",
+      path: "/location/",
+    });
+    sections.push({
+      label: "Inbox",
+      icon: "/assets/icons/inbox.svg",
+      path: "/orders/",
+    });
+  }
+
+  sections.push({
+    label: "Settings",
+    icon: "/assets/icons/settings.svg",
+    path: "/settings/",
+  });
+
+  // const sections = [
+  // 	{ label: "Home", icon: '/assets/icons/home.svg', path: "/dashboard/" },
+  // 	{ label: "Location", icon: '/assets/icons/location.svg', path: "/location/" },
+  // 	{ label: "Inbox", icon: '/assets/icons/inbox.svg', path: "/orders/" },
+  // 	{ label: "Shop", icon: '/assets/icons/shop.svg', path: "" },
+  // 	{ label: "Settings", icon: '/assets/icons/settings.svg', path: "/settings/" },
+  // ];
+  return (
+    <div className="nav__container">
+      {sections.map((section, key) => {
+        return (
+          <Link key={key} to={section.path}>
+            <Tooltip target=".custom-target-icon" />
+            <img
+              data-pr-tooltip={section.label}
+              data-pr-position="right"
+              data-pr-at="right+5 top"
+              data-pr-my="left center-2"
+              className="custom-target-icon"
+              key={section.label}
+              src={section.icon}
+            ></img>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Nav;
