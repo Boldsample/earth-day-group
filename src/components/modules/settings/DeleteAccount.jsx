ec771db1bc4683d95bf0315466bc534f9e41e8c7
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-07-17 20:18:14 -0500

    minor style changes in DeleteAccount component

--- src/components/modules/settings/DeleteAccount.jsx
+++ src/components/modules/settings/DeleteAccount.jsx
@@ -4,6 +4,9 @@ import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
 import { PasswordInput } from '@ui/forms';
 import { useForm } from 'react-hook-form'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan} from '@fortawesome/free-solid-svg-icons'
+
 
 const DeleteAccount = () => {
 const [visible, setVisible] = useState(false);
@@ -29,17 +32,26 @@ const footerContent = (
     </div>
 );
 
+const headerContent = (
+  <div>
+   {/* <div className="icon trash text-red-state"><FontAwesomeIcon icon={faTrashCan} style={{fontSize: "60px"}} /></div> */}
+  </div>
+);
+
+
+
+
   return (
     <div className="layout" style={{background: 'white'}}>
     <div className="main__content centerwidth alignttop">
     <div className="settings__card">
-        <h4 className='internal mb-2 text-gray'>Disclaimer: Account Deletion</h4>
+        <h4 className='mb-2 text-green-earth'>Disclaimer: Account Deletion</h4>
         <p className='text-left mb-2'>By deleting your account, you agree to the following:</p>
         <ul className='text-left ml-1'>
-            <li className='mb-1'><b>Permanent Deletion:</b> Your account and all associated data will be permanently removed and cannot be recovered.</li>
-            <li  className='mb-1'><b>Loss of Access:</b> You will lose access to all services, content, and features linked to your account.</li>
-            <li  className='mb-1'><b>Data Responsibility:</b> Ensure you back up any data you want to keep, as we are not liable for any loss of data.</li>
-            <li  className='mb-1'><b>Outstanding Transactions:</b> Resolve any pending transactions or issues before deletion.</li>
+            <li className='mb-1'><b className='text-green-earth'>Permanent Deletion:</b> Your account and all associated data will be permanently removed and cannot be recovered.</li>
+            <li  className='mb-1'><b className='text-green-earth'>Loss of Access:</b> You will lose access to all services, content, and features linked to your account.</li>
+            <li  className='mb-1'><b className='text-green-earth'>Data Responsibility:</b> Ensure you back up any data you want to keep, as we are not liable for any loss of data.</li>
+            <li  className='mb-1'><b className='text-green-earth'>Outstanding Transactions:</b> Resolve any pending transactions or issues before deletion.</li>
         </ul>
         <p className='text-left mt-2'>By proceeding, you accept these terms and understand the permanent consequences.</p>
         {/* <Link  to={'/settings/edit/'} className="button red-state">Delete your account</Link> */}
@@ -48,10 +60,11 @@ const footerContent = (
             label='Delete your account'
             onClick={()=> setVisible(true)}
        />
-       <Dialog header="Are you sure?" visible={visible} style={{ width: '50vw', textAlign: 'center' }} onHide={() => {if (!visible) return; setVisible(false); }} footer={footerContent}>
-       <p>Do you really want to delete your account? This process cannot be undone.</p>
+       <Dialog header={headerContent} visible={visible} style={{ width: '50vw', textAlign: 'center' }} onHide={() => {if (!visible) return; setVisible(false); }} footer={footerContent}>
+      <div className="icon trash text-red-state"><FontAwesomeIcon icon={faTrashCan} style={{fontSize: "60px", paddingBottom: "20px"}} /></div>
+       <p style={{fontSize:'20px', fontWeight: '500'}}>Do you really wish to delete your account? <br /> This process cannot be undone.</p>
        <PasswordInput
-            width="100%"
+            width="50%"
             maxLength={20}
             label="Password"
             showLabel={false}
