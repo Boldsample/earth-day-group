28f4c91974c96f432a71b22c8f3c1d9aa7c7f705
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-08-11 21:16:43 -0500

    add first img uploader component to Configure ads

--- src/components/modules/admin/ConfigureAds.jsx
+++ src/components/modules/admin/ConfigureAds.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { FileUpload } from 'primereact/fileupload';
 
 const ConfigureAds = () => {
     const user = useSelector((state) => state.users.userData)
@@ -8,7 +9,12 @@ const ConfigureAds = () => {
     <div className='layout'>
         <img className="layout__background" src="/assets/full-width.svg" />
         <div className={'main__content fullwidth ' + (user.role == 'user' ? 'useroffers' : '')}>
-        <h1>hello world</h1>
+            <div className='fullwidth'>
+                <h1>hello world</h1>
+                <div>
+                <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>} />
+                </div>
+            </div>
         </div>
     </div>
   )
