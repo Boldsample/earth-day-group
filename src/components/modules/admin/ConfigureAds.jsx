e92da868b819f15396cdb4ba53bce7e63bc0df75
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-08-12 21:26:35 -0500

    add dialog to ConfigureAds component

--- src/components/modules/admin/ConfigureAds.jsx
+++ src/components/modules/admin/ConfigureAds.jsx
@@ -1,13 +1,15 @@
-import React, {useRef} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { FileUpload } from 'primereact/fileupload';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 import { useForm } from "react-hook-form"
+import { Dialog } from 'primereact/dialog';
 
 import "./styles.sass"
 
 const ConfigureAds = () => {
+    const [visible, setVisible] = useState(false);
     const user = useSelector((state) => state.users.userData)
     const {
       watch,
@@ -48,9 +50,17 @@ const ConfigureAds = () => {
         <div className={'main__content fullwidth ' + (user.role == 'user' ? 'useroffers' : '')}>
           <div className="ads__container">
             <div className='fullwidth'>
+            <Dialog header="Header" visible={visible} style={{ width: '50vw' }} onHide={() => {if (!visible) return; setVisible(false); }}>
+                <p className="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </Dialog>
               <div className="flex flex-start mb-1">
                 <h4 className='title__noWidth'>Main Header Banner</h4>
-                <FontAwesomeIcon className='ml-1' color='var(--dark-blue)' icon={faCircleInfo} fontSize="25px" />
+                <button className='info__btn' onClick={() => setVisible(true)}><FontAwesomeIcon color='var(--dark-blue)' icon={faCircleInfo} fontSize="25px" /></button>
               </div>
                 <p>This banner will appear at the top of the user's screen in a horizontal layout.</p>
                 <div>
