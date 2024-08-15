554d233f481a30b5a4fd5aa4ba3cb7db42f1a1ec
JuanJacoboViera <juanjacoboviera@gmail.com>
2024-08-14 20:33:03 -0500

    create AdManager component

--- src/components/modules/admin/configureAds/AdManager.jsx
+++ src/components/modules/admin/configureAds/AdManager.jsx
@@ -3,41 +3,85 @@ import "./styles.sass"
 import { TextInput, MultiSelectInput, CalendarInput } from "@ui/forms"
 import { Calendar } from 'primereact/calendar';
 import { useForm } from "react-hook-form"
+import { useDispatch, useSelector } from 'react-redux'
+import { Dialog } from 'primereact/dialog';
+import { FileUpload } from 'primereact/fileupload';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 
-const ContentBox = () => {
-  const [date, setDate] = useState(null);
-  const {
-    watch,
-    reset,
-    control,
-    setValue,
-    setFocus,
-    setError,
-    getValues,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      ad_name: "",
-      ad_url: "",
-      ad_target: "",
-      ad_duration: "",
-    },
-  })
-  const modules = [
-    { name: 'Users' },
-    { name: 'Companies' },
-    { name: 'Foundations/NGOs'},
+const AdManager = () => {
+    const [visible, setVisible] = useState(false);
+    const user = useSelector((state) => state.users.userData)
+    const [bannerImg, setBannerImg] = useState(null)
+    const [date, setDate] = useState(null);
+    const {
+        watch,
+        reset,
+        control,
+        setValue,
+        setFocus,
+        setError,
+        getValues,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: {
+        ad_name: "",
+        ad_url: "",
+        ad_target: "",
+        ad_duration: "",
+        },
+    })
+    const modules = [
+        { name: 'Users' },
+        { name: 'Companies' },
+        { name: 'Foundations/NGOs'},
+    ];
 
-];
-const onSubmit = async (data) => {
-  console.log(data)
-}
+    const customBase64Uploader = async (event) => {
+        // convert file to base64 encoded
+        const file = event.files[0];
+        const reader = new FileReader();
+        let blob = await fetch(file.objectURL).then((r) => r.blob()); //blob:url
+  
+        reader.readAsDataURL(blob);
+  
+        reader.onloadend = function () {
+            const base64data = reader.result;
+            setBannerImg(base64data)
+            console.log(base64data)
+        };
+    };
 
-  const getFormErrorMessage = (fieldName) => errors[fieldName] && <small className="p-error">{errors[fieldName]?.message}</small>
+    const onSubmit = async (data) => {
+    console.log(data)
+    }
+
+    const getFormErrorMessage = (fieldName) => errors[fieldName] && <small className="p-error">{errors[fieldName]?.message}</small>
+
   return (
+    <>
+    <Dialog header="Header" visible={visible} style={{ width: '50vw' }} onHide={() => {if (!visible) return; setVisible(false); }}>
+        <p className="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+    </Dialog>
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className='p-fileupload p-fileupload-advanced p-component mt-3'>
+    <div className='fullwidth mt-3'>
+            <div className="flex flex-start mb-1">
+              <h4 className='title__noWidth'>Home Menu Banner</h4>
+              <FontAwesomeIcon className='ml-1' color='var(--dark-blue)' icon={faCircleInfo} fontSize="25px" />
+            </div>
+              <p>This banner appears as a rectangular menu button on every user's home screen.</p>
+            {bannerImg == null ? 
+              <div>
+              <FileUpload name="banner_image" customUpload uploadHandler={customBase64Uploader} url={'/api/upload'} accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop the image here to upload.</p>} />
+              </div>
+          :
+          <div className='p-fileupload p-fileupload-advanced p-component mt-3'>
           <div className='p-fileupload p-fileupload-buttonbar'>
               <button type='submit' onClick={handleSubmit(onSubmit)} form='ad_form ' className='green-earth'>Create Ad</button>
               <button className='red-state'>Cancel</button>
@@ -113,9 +157,11 @@ const onSubmit = async (data) => {
           </div>
           </div>
           </div>
+        }
+        </div>
       </form>
-
+    </>
   )
 }
 
-export default ContentBox
+export default AdManager
--- src/components/modules/admin/configureAds/ConfigureAds.jsx
+++ src/components/modules/admin/configureAds/ConfigureAds.jsx
@@ -6,7 +6,7 @@ import { faCircleInfo } from '@fortawesome/free-solid-svg-icons'
 import { useForm } from "react-hook-form"
 import { Dialog } from 'primereact/dialog';
 import ContentBox from '@ui/cards/contentBox/ContentBox';
-
+import AdManager from './AdManager';
 import "../styles.sass"
 
 const ConfigureAds = () => {
@@ -60,37 +60,8 @@ const ConfigureAds = () => {
                 </p>
             </Dialog>
           <div className="ads__container">
-            <div className='fullwidth'>
-              <div className="flex flex-start mb-1">
-                <h4 className='title__noWidth'>Main Header Banner</h4>
-                <button className='info__btn' onClick={() => setVisible(true)}><FontAwesomeIcon color='var(--dark-blue)' icon={faCircleInfo} fontSize="25px" /></button>
-              </div>
-                <p>This banner will appear at the top of the user's screen in a horizontal layout.</p>
-                <div>
-                <FileUpload name="demo[]" url={'/api/upload'} multiple accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0"
-                  watch={watch}
-                  control={control}
-                  setError={setError}
-                  setValue={setValue}
-                  getValues={getValues}
-                >Drag and drop the image here to upload.</p>} />
-                </div>
-                {/* <ContentBox/> */}
-            </div>
-            <div className='fullwidth mt-3'>
-            <div className="flex flex-start mb-1">
-              <h4 className='title__noWidth'>Home Menu Banner</h4>
-              <FontAwesomeIcon className='ml-1' color='var(--dark-blue)' icon={faCircleInfo} fontSize="25px" />
-            </div>
-              <p>This banner appears as a rectangular menu button on every user's home screen.</p>
-            {bannerImg == null ? 
-              <div>
-              <FileUpload name="banner_image" customUpload uploadHandler={customBase64Uploader} url={'/api/upload'} accept="image/*" maxFileSize={1000000} emptyTemplate={<p className="m-0">Drag and drop the image here to upload.</p>} />
-              </div>
-          :
-          <ContentBox/> 
-        }
-        </div>
+            <AdManager/>
+            <AdManager/>
           </div>
         </div>
     </div>
