d605d4062da6a701c09daf50a6898e5e00149cc6
reneorozcome <natosrene@gmail.com>
2024-12-11 17:21:33 -0500

    The problem with filters and search in the chat list has been resolved

--- src/components/modules/chat/Chats.jsx
+++ src/components/modules/chat/Chats.jsx
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Button } from 'primereact/button'
 import { useEffect, useState } from 'react'
 import { InputText } from 'primereact/inputtext'
@@ -14,34 +14,43 @@ import ChatSkeleton from '@ui/skeletons/chatSkeleton/ChatSkeleton'
 import { Paginator } from 'primereact/paginator'
 
 const Chats = () => {
+  const dispatch = useDispatch()
   const [users, setUsers] = useState([])
-  const [filters, setFilters] = useState([])
   const skeletonPlaceHolder = ["", "", "", ""]
+  const [resetFields, setResetFields] = useState(true)
   const user = useSelector(state => state.users.userData)
   const [page, setPage] = useState({first: 0, page: 0, rows: 4})
+  const [filters, setFilters] = useState({role: [], keyword: ''})
   const [t] = useTranslation('translation', { keyPrefix: 'chat.chats' })
 
-  const updateFilter = filter => {
+  const updateFilters = (name, value, wait=false) => {
     setFilters(prev => {
-      if(prev.includes(filter))
-        return prev.filter(item => item !== filter);
-      else
-        return [...prev, filter];
+      let setValue = value
+      if(name == 'role')
+        setValue = prev.role.includes(value) ? prev.role.filter(item => item !== value) : [...prev.role, value]
+      return {...prev, [name]: setValue}
     })
+    setResetFields(true)
   }
-  const getUserList = async () => {
-    let _filter = { user: `u.id<>'${user?.id}' AND (c.incoming='${user?.id}' OR c.outgoing='${user?.id}')` }
-    if(filters?.length > 0)
-      _filter['role'] = "(role='" + filters.join("' OR role='") + "')"
+  
+  const callUsers = async (ex = false) =>{
+    let _filter = { user: `u.id<>'${user?.id}' AND (c.incoming='${user?.id}' OR c.outgoing='${user?.id}') AND u.state=1` }
+    if(filters?.role?.length)
+      _filter['role'] = `(u.role='${filters?.role?.join("' OR u.role='")}')`
+    if(filters?.keyword != '')
+      _filter['keyword'] = encodeURIComponent(`(u.name LIKE '%${filters.keyword}%' OR u.email LIKE '%${filters.keyword}%')`)
     const _users = await getUsers(_filter, 'min', user?.id, page)
-    setUsers(_users);
+    setUsers(_users)
   }
 
   useEffect(() => {
-    getUserList()
-  }, [filters, page])
-	useEffect(() => {
-    setHeader('user')
+    if(resetFields){
+      callUsers()
+      setResetFields(false)
+    }
+  }, [page, resetFields])
+  useEffect(() => {
+    dispatch(setHeader('user'))
   }, [user])
   
   return <div className="layout">
@@ -50,17 +59,17 @@ const Chats = () => {
       <div className="edg-search mb-0">
         <h1 className="text-defaultCase mb-1">{t('mainTitle')}</h1>
         <div className="mb-1">
-          <Button label={t('usersBtnText')} onClick={() => updateFilter('user')} className={'green-earth small ' + (filters.find(f => f == 'user') ? '' : 'outline')} />
-          <Button label={t('recyclingBtnText')} onClick={() => updateFilter('company')} className={'green-earth small ' + (filters.find(f => f == 'company') ? '' : 'outline')} />
-          <Button label={t('vendorsBtnText')} onClick={() => updateFilter('vendor')} className={'green-earth small ' + (filters.find(f => f == 'vendor') ? '' : 'outline')} />
-          <Button label={t('ngoBtnText')} onClick={() => updateFilter('ngo')} className={'green-earth small ' + (filters.find(f => f == 'ngo') ? '' : 'outline')} />
+          <Button label={t('usersBtnText')} onClick={() => updateFilters('role', 'user')} className={'green-earth small ' + (filters?.role?.find(f => f == 'user') ? '' : 'outline')} />
+          <Button label={t('recyclingBtnText')} onClick={() => updateFilters('role', 'company')} className={'green-earth small ' + (filters?.role?.find(f => f == 'company') ? '' : 'outline')} />
+          <Button label={t('vendorsBtnText')} onClick={() => updateFilters('role', 'vendor')} className={'green-earth small ' + (filters?.role?.find(f => f == 'vendor') ? '' : 'outline')} />
+          <Button label={t('ngoBtnText')} onClick={() => updateFilters('role', 'ngo')} className={'green-earth small ' + (filters?.role?.find(f => f == 'ngo') ? '' : 'outline')} />
         </div>
         <div className="fullwidth p-input-icon-left">
           <FontAwesomeIcon icon={faSearch} />
           <InputText
             placeholder={t('inputSearchPlaceHolder')}
             className="p-inputtext"
-            onChange={(e) => updateFilter(e.target.value)} />
+            onChange={(e) => updateFilters('keyword', e.target.value)} />
         </div>
       </div>
       {typeof users?.data == 'undefined' && 
